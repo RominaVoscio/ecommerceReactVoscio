191bf916a6e43f323a1423f2b2517160050b8781
RominaVoscio <89428394+RominaVoscio@users.noreply.github.com>
2022-03-19 19:32:48 -0300

    Desafío entrega Intermedia ProyectoFinalVoscio

---
+++ src/components/productos.js
@@ -0,0 +1,26 @@
+export const productosIniciales =[
+    {
+        id: '1',
+        nombre: "Arlyt",
+        precio: 200,
+        imagen: "/arlytExpress120.jpg",
+        descripcion:"ARLYT EXPRESS MULTIUSO SOLUCIÓN MULTIPROPÓSITO E ISOTÓNICA PARA TODO TIPO DE LENTES DE CONTACTO BLANDAS. LIMPIADORA, DESINFECTANTE, CONSERVADORA, HUMECTANTE Y DE ENJUAGUE.",
+        categoria: '1',
+    },
+    {
+        id: '2',
+        nombre: "Natura",
+        precio: 300,
+        imagen: "/naturaExpess.jpg",
+        descripcion:"NATURA EXPRESS MULTIUSO SOLUCIÓN MULTIPROPÓSITO E ISOTÓNICA PARA TODO TIPO DE LENTES DE CONTACTO BLANDAS. LIMPIADORA, DESINFECTANTE, CONSERVADORA, HUMECTANTE Y DE ENJUAGUE.",
+        categoria: '2',
+    },
+    {
+        id: '3',
+        nombre: "Renu",
+        precio: 250,
+        imagen: "/renuchico.jpg",
+        descripcion:"RENU PLUS MULTIUSO SOLUCIÓN MULTIPROPÓSITO E ISOTÓNICA PARA TODO TIPO DE LENTES DE CONTACTO BLANDAS. LIMPIADORA, DESINFECTANTE, CONSERVADORA, HUMECTANTE Y DE ENJUAGUE.",
+        categoria: '3',
+    }
+];
